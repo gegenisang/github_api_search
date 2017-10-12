fa522cce0af49d5066b27371a8e5a25872df5334
gegenisang <gegenisang@gmail.com>
2017-10-12 20:18:00 +0800

    incomplete

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-
+  $('#myDropdown').dropdown({
+    onChange: function (value, text, $selectedItem) {
+      console.log(arguments);
+      console.log('value', value);
+      console.log('text', text);
+      console.log('$selectedItem.data("test")', $selectedItem.data("test"));
+    }
+  });
 });
