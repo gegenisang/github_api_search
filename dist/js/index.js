e5fb68ad7c881d9dae41c32bdfe55b6e588c7275
gegenisang <gegenisang@gmail.com>
2017-10-13 10:51:43 +0800

    incomplete

--- dist/js/index.js
+++ dist/js/index.js
@@ -13,7 +13,30 @@ $(document).ready(function () {
 
 
   function getGitHub(res) {
-    var url = "https://api.github.com/search/repositories?q=language:" + res + "&sort=stars&order=desc&page=2&";
-    console.log(url);
+    var baseUrl = "https://api.github.com/search/repositories?q=language:" + res + "&sort=stars&order=desc";
+    var dataSet = [];
+    for (var i = 1; i <= 34; i++) {
+      var url = baseUrl + "&page=" + i;
+      // urls.push(url + "&page=" + i);
+      console.log('url', url);
+      $.ajax({
+        url: url,
+        dataType: "json",
+        type: "GET",
+        async: "false",
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', 'token c11e3bccc83d77e388a20034c505155b862e7218');
+        },
+        success: function (data) {
+          $("#list").html("");
+          console.log(data.items.length);
+          dataSet.push(data.items);
+          console.log(dataSet);
+        },
+        error: function (errorMessage) {
+          alert("error");
+        }
+      })
+    }
   }
 });
