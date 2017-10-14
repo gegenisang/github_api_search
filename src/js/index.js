f88d8ebc8b41846c2fe35c2a76ad8d41af200aaa
gegenisang <gegenisang@gmail.com>
2017-10-14 21:44:13 +0800

    incomplete

--- src/js/index.js
+++ src/js/index.js
@@ -13,30 +13,35 @@ $(document).ready(function () {
 
 
   function getGitHub(res) {
-    var baseUrl = "https://api.github.com/search/repositories?q=language:" + res + "&sort=stars&order=desc";
-    var dataSet = [];
-    for (var i = 1; i <= 34; i++) {
-      var url = baseUrl + "&page=" + i;
-      // urls.push(url + "&page=" + i);
-      console.log('url', url);
-      $.ajax({
-        url: url,
-        dataType: "json",
-        type: "GET",
-        async: "false",
-        beforeSend: function (xhr) {
-          xhr.setRequestHeader('Authorization', 'token c11e3bccc83d77e388a20034c505155b862e7218');
-        },
-        success: function (data) {
-          $("#list").html("");
-          console.log(data.items.length);
-          dataSet.push(data.items);
-          console.log(dataSet);
-        },
-        error: function (errorMessage) {
-          alert("error");
+    // var baseUrl = "https://api.github.com/search/repositories?q=language:" + res + "&sort=stars&order=desc";
+    var url = "https://api.github.com/search/repositories?q=language:" + res + "&sort=stars&order=desc";
+    // var dataSet = [];
+    // for (var i = 1; i <= 2; i++) {
+    //   var url = baseUrl + "&page=" + i;
+    //   // urls.push(url + "&page=" + i);
+    //   console.log('url', url);
+    $.ajax({
+      url: url,
+      dataType: "json",
+      type: "GET",
+      async: "false",
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 'token c11e3bccc83d77e388a20034c505155b862e7218');
+      },
+      success: function (data) {
+        $(".list").html("");
+        console.log(data.items.length);
+        console.log(data.items);
+        // dataSet.push(data.items);
+        for (var i = 0; i < data.items.length; i++) {
+          $(".list").append("<li class='item clearfix'><div class='pull-left'><h2><a href='javascript:;'>" + data.items[i].full_name + "</a></h2><p class='description'>" + data.items[i].description + "</p><p class='time'>" + "updata : " + data.items[i].updated_at + "</p></div>" + "<div class='pull-right'><span class='download'><button type='button'><i class='fa fa-cloud-download'></i>Download</button></span><span><i class='fa fa-star'></i><em class='numbers'>" + data.items[i].stargazers_count + "</em></span></div></li>");
         }
-      })
-    }
+
+      },
+      error: function (errorMessage) {
+        console.log("aaa");
+      }
+    })
   }
+  // }
 });
