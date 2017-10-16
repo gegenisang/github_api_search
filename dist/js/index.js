908592389ac737b7f3fd19213e133323738868d5
gegenisang <gegenisang@gmail.com>
2017-10-16 22:46:48 +0800

    page list incomplete

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   var $val;
   var $math = "stars";
+  var num = 1;
   $('#myDropdown').dropdown({
     onChange: function (value, text, $selectedItem) {
       console.log('value', value);
       console.log('text', text);
       $val = text;
-      getGitHub($val, $math);
+      if (num > 1) {
+        num = 1;
+      }
+      getGitHub($val, $math, num);
     }
   });
 
@@ -33,16 +37,25 @@ $(document).ready(function () {
           break;
       }
       console.log($math);
-      getGitHub($val, $math);
+      if (num > 1) {
+        num = 1;
+      }
+      getGitHub($val, $math, num);
     }
   });
 
+  $(".pagination li a").click(function () {
+    $(".pagination li").removeClass("active");
+    num = $(this).data("value");
+    $(this).parent("li").addClass("active");
+    console.log('num', num);
+    getGitHub($val, $math, num);
+  });
 
 
-
-  function getGitHub(lang, sort) {
+  function getGitHub(lang, sort, idx) {
     $(".list").html("");
-    var url = "https://api.github.com/search/repositories?q=language:" + lang + "&sort=" + sort + "&order=desc&per_page=100";
+    var url = "https://api.github.com/search/repositories?q=language:" + lang + "&sort=" + sort + "&page=" + idx + "&order=desc&per_page=100";
 
     $.ajax({
       url: url,
@@ -53,13 +66,14 @@ $(document).ready(function () {
         xhr.setRequestHeader('Authorization', 'token c11e3bccc83d77e388a20034c505155b862e7218');
       },
       success: function (data) {
+
         console.log(data.items.length);
         console.log(data.items);
 
         for (var i = 0; i < data.items.length; i++) {
           $(".list").append("<li class='item clearfix'><div class='pull-left'><h2><a href='javascript:;'>" + data.items[i].full_name + "</a></h2><p class='description'>" + data.items[i].description + "</p><p class='time'>" + "updata : " + data.items[i].updated_at + "</p></div>" + "<div class='pull-right'><span class='download'><a href=" + data.items[i].url + "/zipball/master" + "><i class='fa fa-cloud-download'></i>Download</a></span><span><i class='fa fa-star'></i><em class='numbers'>" + data.items[i].stargazers_count + "</em></span><span><i class='fa fa-code-fork'></i><em class='fork'>" + data.items[i].forks_count + "</em></span></div></li>");
         }
-
+        $(".page-list").removeClass("hidden");
 
       },
       error: function (errorMessage) {
